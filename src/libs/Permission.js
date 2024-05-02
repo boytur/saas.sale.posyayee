3392007b1bd25e8df0eb6dafa5e8b5fb2c9b91a8
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-03 00:23:40 +0700

    feat(permissiom): funtion to check permision

---
+++ src/libs/Permission.js
@@ -0,0 +1,83 @@
+import { getUuid } from "./localStrage";
+import userDecode from "./userDecode";
+
+class Permission {
+
+    constructor() {
+        this.role = (userDecode(getUuid()))?.user?.user_role || '';
+        this.user_acc_verify = (userDecode(getUuid()))?.user?.user_acc_verify || '';
+    }
+
+    canGetOrderSessionId() {
+        if (this.role === "owner" && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canLogout() {
+        if (this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canViewHistory() {
+        if ((this.role === 'owner' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canViewStock() {
+        if ((this.role === 'owner' || this.role === 'employee' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canViewDashboard() {
+        if ((this.role === 'owner' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+
+    canViewFinance() {
+        if ((this.role === 'owner' || this.role === 'employee' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canSettingOwnAccount() {
+        if ((this.role === 'owner' || this.role === 'employee' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canSettingStore() {
+        if ((this.role === 'owner' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canSellProducts() {
+        if ((this.role === 'owner' || this.role === 'employee' || this.role === 'manager') && this.user_acc_verify) {
+            return true;
+        }
+        return false;
+    }
+
+    canManageEmployees() {
+        if (this.role === 'god' || this.role === 'owner' || this.role === 'manager') {
+            return true;
+        }
+        return false;
+    }
+}
+
+export default Permission;
---
+++ src/libs/checkRoutePermission.js
@@ -0,0 +1,26 @@
+import Permission from "./Permission";
+
+const permission = new Permission();
+
+const checkRoutePermission = (route) => {
+    switch (route) {
+        case "/":
+            return permission.canSellProducts();
+        case "/history":
+            return permission.canViewHistory();
+        case "/product":
+            return permission.canViewStock();
+        case "/finance":
+            return permission.canViewFinance();
+        case "/dashboard":
+            return permission.canViewDashboard();
+        case "/setting/account":
+            return permission.canSettingOwnAccount();
+        case "/setting/store":
+            return permission.canSettingStore();
+        default:
+            return false;
+    }
+};
+
+export default checkRoutePermission;
