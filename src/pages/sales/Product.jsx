2b03a71597f5f4cbcbc65b487bb22212612e566c
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-04 01:16:40 +0700

    refactor(product): added categories from real data

--- src/pages/sales/Product.jsx
+++ src/pages/sales/Product.jsx
@@ -1,12 +1,12 @@
 import { CiSearch } from "react-icons/ci";
 /* eslint-disable react/prop-types */
-function Product({ products }) {
+function Product({ products, categories }) {
 
-  let productNobarcode = products?.filter((p) => p.prod_barcode === "1234567890123");
+  let productNobarcode = products?.filter((p) => p.prod_barcode === "" || p.prod_barcode === null);
 
   return (
     <div className="w-full h-full rounded-md p-1 bg-white">
-      <div className="h-full overflow-auto flex justify-evenly flex-wrap gap-2">
+      <div className="h-full overflow-auto flex justify-evenly flex-wrap md:gap-2 gap-[3px] pb-12">
         <div className="w-full">
           <div className="flex gap-2 mx-2">
             <div className="w-full">
@@ -14,36 +14,35 @@ function Product({ products }) {
               <div className="relative flex items-center w-full">
                 <input
                   placeholder="กรอกรายละเอียด....."
-                  className="input-primary md:text-lg text-[0.8rem] w-full md:h-[3rem] mb-2 rounded-lg px-2 md:pl-12 pl-6">
+                  className="input-primary md:text-lg text-[0.8rem] w-full md:h-[2.5rem] mb-2 rounded-lg px-2 md:pl-9 pl-6">
                 </input>
-                <CiSearch className=" absolute md:pl-3 text-[1.3rem] md:text-[2.5rem] pl-2 mb-2" />
+                <CiSearch className=" absolute md:pl-3 text-[1.3rem] md:text-[2rem] pl-2 mb-2" />
               </div>
             </div>
 
             {/* categories */}
             <div className="w-2/4 ">
               <label className="text-[0.7rem]  ml-1 text-gray-500" htmlFor="">ค้นหาจาก</label>
-              <select className="input-primary md:h-[3rem] text-[0.8rem] md:text-[1rem] cursor-pointer">
-                <option value="">ชื่อ</option>
-                <option value="">บาร์โค้ด</option>
-                <option value="">ราคา</option>
+              <select className="input-primary md:h-[2.5rem] text-[0.8rem] md:text-[1rem] cursor-pointer">
+                <option value="prod_name">ชื่อสินค้า</option>
+                <option value="prod_sale">ราคาสินค้า</option>
               </select>
             </div>
 
             {/* sort */}
-            <div className="w-2/4 ">
+            <div className="w-2/4">
               <label className="text-[0.7rem]  ml-1 text-gray-500" htmlFor="">ประเภท</label>
-              <select className="input-primary md:h-[3rem] text-[0.8rem] md:text-[1rem] cursor-pointer">
-                <option>ประเภท</option>
-                <option value="">1</option>
-                <option value="">3</option>
+              <select className="input-primary md:h-[2.5rem] text-[0.8rem] md:text-[1rem] cursor-pointer">
+                {categories?.map((c) => (
+                  <option key={c.cat_id} value={c.cat_id}>{c.cat_name}</option>
+                ))}
               </select>
             </div>
 
           </div>
         </div>
         {productNobarcode?.map((product) => (
-          <div key={product.prod_id} className="md:w-[8rem] md:-[10rem] h-[10rem] w-[10rem] items-center md:flex md:flex-col rounded-md p-1 bg-white cursor-pointer shadow-md hover:border-primary border-[2px]">
+          <div key={product.prod_id} className="md:w-[8rem] md:h-[10rem] h-[10rem] w-[7.5rem] mb-2 md:mb-0 items-center md:flex md:flex-col rounded-md p-1 bg-white cursor-pointer shadow-md hover:border-primary border-[2px]">
             <div className="md:w-full h-[5rem]">
               <img src={product.prod_image} alt={product.prod_name} className="w-full h-full object-cover" />
             </div>
