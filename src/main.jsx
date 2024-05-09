4aaf60c9df94a6453d524ce99dccaf92555bd151
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-10 03:44:46 +0700

    build: @material-tailwind/react

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import AuthProvider from './contexts/AuthProvider.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from "@material-tailwind/react";
+import theme from './components/themes/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <ThemeProvider value={theme}>
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </BrowserRouter>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+import theme from "./src/components/themes/theme";
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
+  
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    theme
   },
   plugins: [],
-}
+});
