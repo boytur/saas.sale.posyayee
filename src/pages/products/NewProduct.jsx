23d400a4b657d95f50fe808dc3c32a9f548e4156
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-09 11:06:02 +0700

    refactor(product): move pagonation to componment in steed reuse code

--- src/pages/products/NewProduct.jsx
+++ src/pages/products/NewProduct.jsx
@@ -4,12 +4,11 @@ import Swal from "sweetalert2";
 import { useAuth } from "../../contexts/AuthProvider";
 import Permission from "../../libs/Permission";
 import Axios from "../../libs/Axios";
-import { MdOutlineNavigateNext } from "react-icons/md";
-import { MdOutlineNavigateBefore } from "react-icons/md";
 import { LuPencilLine } from "react-icons/lu";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import Barcode from 'react-barcode';
 import formatUTCtoThai from "../../libs/formatUTCtoThai";
+import Pagination from "../../components/pagination/Pagination";
 
 function NewProduct() {
     const { authenticated } = useAuth();
@@ -243,56 +242,12 @@ function NewProduct() {
                         </tbody>
                     </table>
                 </div>
-                {pageState.total > 0 ?
-
-                    <div className="mt-4 flex flex-col gap-2 justify-center items-center pb-12">
-                        <div className="mt-2">
-                            สินค้าทั้งหมด {pageState.total} อย่าง
-                        </div>
-                        <div className="mb-12">
-                            <button
-                                onClick={() => setPageState((prevState) => ({ ...prevState, page: Math.max(1, prevState.page - 1) }))}
-                                disabled={pageState.page === 1}
-                                className="font-bold py-2 px-3 rounded-md border">
-                                <MdOutlineNavigateBefore />
-                            </button>
-                            <span className="ml-2">
-                                {Array.from({ length: Math.min(6, totalPages) }, (_, i) => {
-                                    const pageNumber = i + 1;
-                                    return (
-                                        <button
-                                            key={pageNumber}
-                                            onClick={() => setPageState((prevState) => ({ ...prevState, page: pageNumber }))}
-                                            className={`py-2 px-3 rounded-md border ${pageNumber === pageState.page ? 'bg-[#E4E3FF] text-primary' : ''}`}>
-                                            {pageNumber}
-                                        </button>
-                                    );
-                                })}
-                                {totalPages > 6 && <span className="py-2 px-3">...</span>}
-                            </span>
-                            <button
-                                onClick={() =>
-                                    setPageState((prevState) => ({ ...prevState, page: Math.min(totalPages, prevState.page + 1) }))
-                                }
-                                disabled={pageState.page === totalPages}
-                                className="font-bold py-2 px-3 rounded-md ml-2 border">
-                                <MdOutlineNavigateNext />
-                            </button>
-                        </div>
-                    </div>
-                    :
-                    <div className="w-full flex flex-col items-center mt-5 justify-center">
-                        {pageState.loading ?
-                            <div className="flex justify-center mt-12">
-                                <div className="loader w-full"></div>
-                            </div>
-                            :
-                            <>
-                                <p>ร้านของคุณยังไม่มีสินค้าใหม่ค่ะ</p>
-                            </>
-                        }
-                    </div>
-                }
+                <Pagination
+                    totalPages={totalPages}
+                    pageState={pageState}
+                    setPageState={setPageState}
+                    lable={`รายการสินค้าใหม่ ${pageState.total} รายการ`}
+                />
             </>
         </>
     )
