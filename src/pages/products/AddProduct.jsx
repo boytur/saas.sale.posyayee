d8a3f8296a7ce779cfb599e47a2b11b8e698d618
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-10 10:24:16 +0700

    test: added spam add product testing server performance

--- src/pages/products/AddProduct.jsx
+++ src/pages/products/AddProduct.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
-
+import Axios from "../../libs/Axios";
+import { faker } from '@faker-js/faker';
 
 const StyledDiv = styled.div`
   height: calc(100vh - 5rem); /* Default height */
@@ -11,11 +11,37 @@ const StyledDiv = styled.div`
 `;
 
 function AddProduct() {
-    return (
-        <StyledDiv>
-            add product
-        </StyledDiv>
-    )
+
+  const handleAddProduct = async () => {
+    try {
+      const randomNumber = Math.floor(Math.random() * 1000);
+      const randomNumber2 = Math.floor(Math.random() * 1000);
+      const randomNumber3 = Math.floor(Math.random() * 100);
+      for (let i = 1; i < 1000; i++) {
+        const payload = {
+          'auto_generate_barcode': `${i % 2 === 0 ? true : false}`,
+          'prod_name': `prod spam ${faker.string.uuid()}`,
+          'prod_cost': randomNumber,
+          'prod_barcode': '',
+          'prod_sale': randomNumber2,
+          'prod_quantity': randomNumber3,
+          'cat_id': 1,
+          'unit_id': 1
+        };
+
+        const response = await Axios.post('/api/product/add-product', payload);
+        console.log("Product added successfully:", response.data);
+      }
+    } catch (err) {
+      console.error("Error while adding products: ", err);
+    }
+  }
+  handleAddProduct();
+  return (
+    <StyledDiv>
+      add product
+    </StyledDiv>
+  )
 }
 
 export default AddProduct
