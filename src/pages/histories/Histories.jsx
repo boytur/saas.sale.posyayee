74c5bce27687200496e574cc8036ec9801f37ccc
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-19 17:04:07 +0700

    ✨ feat[History]: added ui for history page and connect api endpoint (not sort yet)

--- src/pages/histories/Histories.jsx
+++ src/pages/histories/Histories.jsx
@@ -1,12 +1,364 @@
-import StyledDiv from "../../components/styleDiv/StyledDiv";
-function Histories() {
-  document.title = "ประวัติการขาย 💸 POSYAYEE";
-  
+import Swal from 'sweetalert2'
+import StyledDiv from '../../components/styleDiv/StyledDiv'
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2'
+import Axios from '../../libs/Axios.js'
+import { useEffect, useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { Card } from '@material-tailwind/react'
+import formatUTCtoThaiWithTime from '../../libs/formatUTCtoThaiWithTime.js'
+import Pagination from '../../components/pagination/Pagination.jsx'
+
+const TABLE_HEAD = [
+  'ลำดับ',
+  'เลขบิล',
+  'ยอดทั้งหมด',
+  'ส่วนลด',
+  'กำไร',
+  'การชำระเงิน',
+  'เวลาขาย',
+  'คนขาย'
+]
+
+function Histories () {
+  document.title = 'ประวัติการขาย 💸 POSYAYEE'
+  const navigate = useNavigate()
+
+  const [searchParams] = useSearchParams({
+    page: 1,
+    perPage: 10
+  })
+
+  const [start, setStart] = useState(() => {
+    if (!searchParams.get('start')) {
+      const currentDate = new Date()
+      currentDate.setUTCHours(17, 0, 0, 0)
+      currentDate.setDate(currentDate.getDate() - 1)
+      return currentDate.toISOString()
+    } else {
+      return searchParams.get('start')
+    }
+  })
+
+  const [end, setEnd] = useState(() => {
+    if (!searchParams.get('end')) {
+      const currentDate = new Date()
+      return currentDate.toISOString()
+    } else {
+      return searchParams.get('end')
+    }
+  })
+
+  const [pageState, setPageState] = useState({
+    data: [],
+    category: [],
+    page: searchParams.get('page') || 1,
+    pageSize: searchParams.get('perPage') || 100,
+    total: 0,
+    loading: false
+  })
+
+  const fetchBillHistory = async () => {
+    try {
+      setPageState(prevState => ({
+        ...prevState,
+        loading: true
+      }))
+
+      const response = await Axios.get('/api/analytic/products/bill', {
+        params: {
+          page: pageState.page,
+          perPage: pageState.pageSize,
+          start: start,
+          end: end
+        }
+      })
+
+      if (response.status === 200) {
+        setPageState(prevState => ({
+          ...prevState,
+          data: response.data,
+          total: response.data.total,
+          loading: false
+        }))
+      }
+    } catch (err) {
+      console.error(err)
+      setPageState(prevState => ({
+        ...prevState,
+        loading: false
+      }))
+      Swal.fire({
+        icon: 'error',
+        title: 'เกิดข้อผิดพลาด',
+        text: err.response?.data?.message || 'กรุณาลองใหม่อีกครั้ง'
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchBillHistory()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageState.page, pageState.pageSize])
+
+  useEffect(() => {
+    const newSearchParams = new URLSearchParams()
+
+    newSearchParams.set('page', pageState.page)
+    newSearchParams.set('perPage', pageState.pageSize)
+    newSearchParams.set('start', start)
+    newSearchParams.set('end', end)
+
+    navigate(`/history?${newSearchParams}`)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageState.pageSize, pageState.page])
+
+  const totalPages = Math.ceil(pageState.total / pageState.pageSize)
+
+  const handlePageSizeChange = event => {
+    const newSize = parseInt(event.target.value)
+    setPageState(prevState => ({ ...prevState, pageSize: newSize }))
+  }
+
+  const setStartDateFromDatePickup = start => {
+    const newStartDate = new Date(start)
+    newStartDate.setUTCHours(17, 0, 0, 1)
+    newStartDate.setDate(newStartDate.getDate() - 1)
+    setStart(newStartDate.toISOString())
+  }
+
+  const setEndDateFromDatePickup = end => {
+    const newEndDate = new Date(end)
+    newEndDate.setUTCHours(16, 59, 59, 59)
+    setEnd(newEndDate.toISOString())
+  }
+
   return (
-    <StyledDiv className="w-full bg-white rounded-md">
-      Histories
+    <StyledDiv className='w-full rounded-md overflow-y-scroll'>
+      <div className='rounded-md'>
+        <div className='bg-white shadow-sm rounded-md p-2 lg:flex lg:flex-row justify-between'>
+          <div className='pb-3 lg:mb-0 mt-1'>
+            <h1 className='text-primary font-bold text-[1.4rem]'>
+              ประวัติการขาย
+            </h1>
+          </div>
+        </div>
+
+        <div className='lg:flex lg:flex-row lg:justify-between flex flex-col gap-2 items-center bg-white mt-1 pb-4 p-1 rounded-md'>
+          <div className='lg:flex lg:flex-row lg:w-3/4 w-full gap-2'>
+            <div className='w-full'>
+              <label htmlFor='' className='text-[0.7rem] ml-1 text-black/80'>
+                วันที่เริ่ม
+              </label>
+              <input
+                onChange={e => setStartDateFromDatePickup(e.target.value)}
+                type='date'
+                className=' border border-gray-300 rounded-md p-1 py-[7px] input-primary cursor-pointer'
+              />
+            </div>
+            <div className='w-full'>
+              <label htmlFor='' className='text-[0.7rem] ml-1 text-black/80'>
+                วันที่สิ้นสุด
+              </label>
+              <input
+                onChange={e => setEndDateFromDatePickup(e.target.value)}
+                type='date'
+                className='border border-gray-300 rounded-md p-1 py-[7px] input-primary cursor-pointer'
+              />
+            </div>
+
+            {/*  */}
+            <div className='w-full'>
+              <label htmlFor='' className='text-[0.7rem] ml-1 text-black/80'>
+                เรียงตาม
+              </label>
+              <select name='' className='input-primary cursor-pointer' id=''>
+                <option value=''>ทุกคน</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+              </select>
+            </div>
+
+            {/*  */}
+            <div className='w-full'>
+              <label htmlFor='' className='text-[0.7rem] ml-1 text-black/80'>
+                เรียงจาก
+              </label>
+              <select name='' className='input-primary cursor-pointer' id=''>
+                <option value=''>ทุกคน</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+              </select>
+            </div>
+
+            {/*  */}
+            <div className='w-full'>
+              <label htmlFor='' className='text-[0.7rem] ml-1 text-black/80'>
+                คนขาย
+              </label>
+              <select name='' className='input-primary cursor-pointer' id=''>
+                <option value=''>ทุกคน</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+                <option value=''>user-1</option>
+              </select>
+            </div>
+
+            {/*  */}
+            <div className='w-full lg:w-fit'>
+              <label htmlFor='' className='text-[0.7rem] text-white ml-1'>
+                .
+              </label>
+              <button
+                onClick={() => fetchBillHistory()}
+                className='w-full justify-center btn-primary px-5 py-[0.42rem] flex items-center gap-1'
+              >
+                <HiOutlineMagnifyingGlass />
+                <p>ค้นหา</p>
+              </button>
+            </div>
+          </div>
+
+          {/* pagePer */}
+          <div className='lg:block lg:w-fit flex lg:mt-0 mt-2 justify-end w-full'>
+            <label htmlFor='' className='text-[0.7rem] text-white ml-1'>
+              .
+            </label>
+            <div className='flex items-center gap-2'>
+              <p>แสดง:</p>
+              <select
+                value={pageState.pageSize}
+                onChange={handlePageSizeChange}
+                className='border border-gray-300 rounded-md py-2  cursor-pointer input-primary'
+              >
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+                <option value={20}>20</option>
+                <option value={30}>30</option>
+                <option value={40}>40</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <Card className='w-full overflow-x-scroll'>
+          <table className='w-full border-collapse'>
+            <thead className='h-[3rem] rounded-t-md'>
+              <tr className='text-sm md:text-md'>
+                {TABLE_HEAD.map(head => (
+                  <th
+                    key={head}
+                    className='border-b border-blue-gray-100 bg-blue-gray-50 p-4'
+                  >
+                    <p className='font-normal leading-none'>{head}</p>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className='bg-white'>
+              {pageState?.data?.biils?.map(
+                (
+                  {
+                    bill_no,
+                    bill_all_amount,
+                    bill_all_discount,
+                    bill_all_profit,
+                    bill_payment_method,
+                    createdAt,
+                    user
+                  },
+                  index
+                ) => {
+                  return (
+                    <tr
+                      style={{
+                        height: `50px`
+                      }}
+                      key={index}
+                      className='hover:bg-[#f5f5ff] border-b-[1px] text-center cursor-pointer text-black'
+                    >
+                      <td className='px-3 py-1'>{index + 1}</td>
+                      <td className='px-3 py-1'>
+                        <div className='flex justify-center items-center z-0'>
+                          <div className='text-black/70 text-sm'>{bill_no}</div>
+                        </div>
+                      </td>
+                      <td className='py-1 font-bold text-primary text-lg'>
+                        ฿{bill_all_amount.toFixed(2)}
+                      </td>
+                      <td className='py-1'>{bill_all_discount.toFixed(2)}</td>
+                      <td
+                        className={`py-1 font-bold ${
+                          bill_all_profit > 0
+                            ? 'text-green-700'
+                            : 'text-red-700'
+                        }`}
+                      >
+                        {bill_all_profit.toFixed(2)}
+                      </td>
+                      <td className='py-1 p-2'>
+                        <div className='flex items-center justify-center'>
+                          {bill_payment_method === 'cash' ? (
+                            <>
+                              <p className='border w-fit px-4 rounded-full bg-green-600 text-white text-sm p-1'>
+                                เงินสด
+                              </p>
+                            </>
+                          ) : (
+                            <>
+                              <p className='border w-fit px-4 rounded-full bg-gray-500 text-white text-sm p-1'>
+                                เงินเชื่อ
+                              </p>
+                            </>
+                          )}
+                        </div>
+                      </td>
+                      <td
+                        style={{
+                          maxWidth: '300px',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          whiteSpace: 'nowrap'
+                        }}
+                        className='py-1 px-2'
+                      >
+                        {formatUTCtoThaiWithTime(createdAt)}
+                      </td>
+                      <td className='py-1 mr-2'>
+                        {user?.user_fname ? user.user_fname : user.user_phone}
+                      </td>
+                    </tr>
+                  )
+                }
+              )}
+            </tbody>
+          </table>
+        </Card>
+
+        {pageState.loading && (
+          <div className='flex justify-center w-full mt-12'>
+            <div className='loader'></div>
+          </div>
+        )}
+
+        <Pagination
+          totalPages={totalPages}
+          pageState={pageState}
+          setPageState={setPageState}
+          lable={`ประวัติการขาย ${
+            pageState.page * pageState.pageSize - pageState.pageSize + 1
+          } - ${Math.min(
+            pageState.page * pageState.pageSize,
+            pageState.total
+          )} จาก ${pageState.total} รายการ`}
+        />
+      </div>
     </StyledDiv>
-  );
+  )
 }
 
-export default Histories;
+export default Histories
