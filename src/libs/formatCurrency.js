c59014c2f37a9dd8224aca9ae02db4a46bc812bb
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-28 12:30:21 +0700

    ✨feat[lib]: add formatCurrency utility function

---
+++ src/libs/formatCurrency.js
@@ -0,0 +1,8 @@
+const formatCurrency = amount => {
+    return amount.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+}
+
+export default formatCurrency
